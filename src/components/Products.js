62deca5b41a2b4ef214f9499b37bce5b05485789
yilfri <yilfri97@hotmail.com>
2021-06-24 15:11:33 -0400

    Edit and List component added

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
 const Products = () => {
-	return <div>Productos JS</div>;
+	return (
+		<>
+			<h2 className="text-center my-5">Products List</h2>
+
+			<table className="table table-striped">
+				<thead className="bg-primary table-dark">
+					<tr>
+						<th scope="col">Name</th>
+						<th scope="col">Price</th>
+						<th scope="col">Actions</th>
+					</tr>
+				</thead>
+				<tbody></tbody>
+			</table>
+		</>
+	);
 };
 
 export default Products;
