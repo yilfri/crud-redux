8ae077f7d05fa8319d21fea9cbcbb9c75ee164a7
yilfri <yilfri97@hotmail.com>
2021-06-24 14:39:43 -0400

    Form created for New Products

--- src/components/NewProduct.js
+++ src/components/NewProduct.js
@@ -2,8 +2,43 @@ import React from 'react';
 
 const NewProduct = () => {
 	return (
-		<div>
-			<h1>New Product</h1>
+		<div className="row justify-content-center mt-5">
+			<div className="col-md-8">
+				<div className="card">
+					<div className="card-body">
+						<h2 className="text-center mb-4 font-weight-bold">Add New Product</h2>
+
+						<form>
+							<div className="form-group">
+								<label>Product Name</label>
+								<input
+									type="text"
+									placeholder="Product Name"
+									className="form-control"
+									name="name"
+								/>
+							</div>
+
+							<div className="form-group">
+								<label>Product Price</label>
+								<input
+									type="number"
+									placeholder="Product Price"
+									className="form-control"
+									name="price"
+								/>
+							</div>
+
+							<button
+								type="submit"
+								className="btn btn-primary font-weight-bold text-uppercase d-block w-100"
+							>
+								Add
+							</button>
+						</form>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
