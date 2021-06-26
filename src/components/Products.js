6cc1d0fdfc5619bd5741392f03f972f2b4234913
yilfri <yilfri97@hotmail.com>
2021-06-26 16:31:26 -0400

    Getting data from API

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+//Redux.
+import { useSelector, useDispatch } from 'react-redux';
+import { getProductsAction } from '../actions/productActions';
 
 const Products = () => {
+	// Use dispatch create function
+	const dispatch = useDispatch();
+
+	// Get products when website load.
+	useEffect(() => {
+		const gettingProducts = () => dispatch(getProductsAction());
+		gettingProducts();
+	}, []);
+
 	return (
 		<>
 			<h2 className="text-center my-5">Products List</h2>
